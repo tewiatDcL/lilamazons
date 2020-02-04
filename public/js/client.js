e8d632f506c377115f661b15c113de4eed9a6ff3
tewi <tewiatDcL@protonmail.ch>
2020-02-05 00:15:37 +1300

    Add registration box

--- public/js/client.js
+++ public/js/client.js
@@ -2,4 +2,16 @@ const socket = io();
 
 $(() => {
     console.log('Page loaded');
+
+
+    //*************************************************************** GUI
+    $('#navbar').on('click', 'a', (e) => {
+        if (e.target.id == 'navbar-register') {
+            if ($('#register').is(':visible')) {
+                $('#register').hide();
+            } else {
+                $('#register').show();
+            }
+        }
+    });
 });
